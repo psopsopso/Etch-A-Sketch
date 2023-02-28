e0a79092fa349a4e3f7e24511e7a52acc2349224
Patrice Sorrant <patrice.sorrant@gmail.com>
2023-02-27 21:13:11 -0500

    Switched to lite-brite mode

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ const container = document.querySelector("#main-container");
 const resetBtn = document.querySelector("#reset-btn");
 const rainbowMode = document.querySelector("#rainbow-mode");
 
+const BASE_WIDTH_HEIGHT = 64;
 resetBtn.addEventListener("click", resetGrid);
 
-const BASE_WIDTH_HEIGHT = 64;
-
-for (let i = 0; i < BASE_WIDTH_HEIGHT * BASE_WIDTH_HEIGHT; i++) {
-  const div = document.createElement("div");
-  div.classList.add("etch-div");
-  container.appendChild(div);
-  div.addEventListener("mouseenter", () => {
-    if (rainbowMode.checked) {
+function initialize() {
+  for (let i = 0; i < BASE_WIDTH_HEIGHT * BASE_WIDTH_HEIGHT; i++) {
+    const div = document.createElement("div");
+    div.classList.add("etch-div");
+    container.appendChild(div);
+    div.addEventListener("mouseenter", () => {
       div.style.backgroundColor = getRandomColor();
-    } else {
-      div.style.backgroundColor = "black";
-    }
-  });
+    });
+  }
 }
 
 function resetGrid() {
   gridSquares = document.querySelectorAll(".etch-div");
   gridSquares.forEach((square) => {
-    square.style.backgroundColor = "lightgray";
+    square.style.backgroundColor = "black";
   });
 }
 
@@ -32,3 +29,5 @@ function getRandomColor() {
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+initialize();
